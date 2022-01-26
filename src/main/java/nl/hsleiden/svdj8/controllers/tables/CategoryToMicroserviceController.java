f9a09a0678290e5ac3df621e4ba245917836a6bb
AntiEevee <eefjekarremans@hotmail.nl>
2022-01-26 13:36:47 +0100

    Restore-Microservice

--- src/main/java/nl/hsleiden/svdj8/controllers/tables/CategoryToMicroserviceController.java
+++ src/main/java/nl/hsleiden/svdj8/controllers/tables/CategoryToMicroserviceController.java
@@ -18,6 +18,7 @@ import java.util.*;
 import static java.lang.System.out;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:8080")
 public class CategoryToMicroserviceController {
     private HttpURLConnection con;
 
@@ -29,11 +30,11 @@ public class CategoryToMicroserviceController {
         List<Category> newRequest = null;
         URL url;
         try {
-            url = new URL("http://localhost:8440/microservice/category/" + specific);
+            url = new URL("http://localhost:8081/microservice/category/" + specific);
             con = (HttpURLConnection) url.openConnection();
             con.setRequestProperty("content-type", "application/json; charset=utf8");
-            con.setRequestMethod(duty);
-            if (!duty.equals("get")) {
+            con.setRequestMethod(duty.toUpperCase());
+            if (!duty.equals("GET")) {
                 addCategoryParameter(category);
             }
             newRequest = formToCategory(readRequest(con), duty, specific);
@@ -75,8 +76,7 @@ public class CategoryToMicroserviceController {
 
     private List<Category> formToCategory(String content, String duty, String specific) {
         JsonArray jsArray;
-
-        if (duty.equals("put") || (duty.equals("get")) && specific.equals("all")) {
+        if (duty.equals("PUT") || (duty.equals("GET")) && !specific.equals("all")) {
             JsonObject item = new Gson().fromJson(content, JsonObject.class);
             jsArray = new JsonArray();
             jsArray.add(item);
@@ -87,35 +87,35 @@ public class CategoryToMicroserviceController {
     }
 
     private List<Category> formListOfCategory(JsonArray jsArray) {
-        ArrayList<Category> grants = new ArrayList<>();
+        ArrayList<Category> categories = new ArrayList<>();
         for (JsonElement item : jsArray) {
-            grants.add((Category) new Gson().fromJson(item, Category.class));
+            categories.add((Category) new Gson().fromJson(item, Category.class));
         }
-        return (List<Category>) grants;
+        return (List<Category>) categories;
     }
 
     @GetMapping(value = "/category/all")
     public List<Category> getAllCategories() {
-        return requestToMicroService("all", "get", new Category());
+        return requestToMicroService("all", "GET", new Category());
     }
 
     @GetMapping(value = "/category/{id}")
     public Category getCategory(@PathVariable final Long id) {
-        return requestToMicroService(id.toString(), "get", new Category()).get(0);
+        return requestToMicroService(id.toString(), "GET", new Category()).get(0);
     }
 
     @PutMapping(value = "/category/{id}")
     public Category editCategory(@RequestBody Category editCategory, @PathVariable Long id) throws Exception {
-        return requestToMicroService(id.toString(), "put", editCategory).get(0);
+        return requestToMicroService(id.toString(), "PUT", editCategory).get(0);
     }
 
     @PutMapping(value = "/category")
     public Category addCategory(@RequestBody Category newCategory) {
-        return requestToMicroService("", "put", newCategory).get(0);
+        return requestToMicroService("", "PUT", newCategory).get(0);
     }
 
     @DeleteMapping("/category/{id}")
     public void deleteCategory(@PathVariable Long id) {
-        requestToMicroService(id.toString(), "delete", new Category()).get(0);
+        requestToMicroService(id.toString(), "DELETE", new Category()).get(0);
     }
 }
